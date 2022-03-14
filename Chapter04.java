8035eff00a59f86eb904d154422a4ee0198206ab
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-03-14 15:50:35 +0000

    [Add]chapter4

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+
+  public static void main(String[] args) {
+    String str1;
+    str1 = "風花雪月";
+    System.out.println(str1);
+    str1 = "花鳥風月";
+    System.out.println(str1);
+    final int num1 = 14;
+    System.out.println(num1);
+  }
+}
