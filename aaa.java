5169a1c364103c452926c7e95ac9e67a954ff2db
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-03-16 08:27:16 +0000

    [Add]aaa

---
+++ aaa.java
@@ -0,0 +1,15 @@
+public class aaa {
+  public static void main(String[] args) {
+    int a = 100;
+    System.out.println(a);
+
+    a++;
+    System.out.println(a);
+
+    boolean b = (a == 50);
+    System.out.println(b);
+
+    b = (a == 101);
+    System.out.println(b);
+  }
+}
